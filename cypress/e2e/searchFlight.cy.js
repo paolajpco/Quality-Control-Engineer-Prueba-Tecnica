4988535f05acc0084b800529bf048fc85b1ebd57
paolajpco <paolajpco@gmail.com>
2026-02-26 08:51:05 -0500

    create searchFlight Cypress test

---
+++ cypress/e2e/searchFlight.cy.js
@@ -0,0 +1,30 @@
+const home = require('../pages/homePage');
+const results = require('../pages/resultsPage');
+const passenger = require('../pages/passengerPage');
+
+describe('Search flight flow', () => {
+  it('searches flights and fills passenger data', () => {
+    cy.viewport(1280, 800);
+    home.visit();
+
+    // fill search form
+    home.setOrigin('Bogotá');
+    home.setDestination('Medellín');
+    home.setDepartDate('2026-03-15');
+    home.setReturnDate('2026-03-20');
+    home.openPassengers();
+    home.selectAdults(2);
+    home.search();
+
+    // wait and select
+    results.waitForResults();
+    results.selectFirstResult();
+
+    // fill passenger and continue
+    passenger.fillPassenger({ firstName: 'Test', lastName: 'User', doc: '12345678' });
+    passenger.continue();
+
+    // verify we progressed to a passenger/checkout page
+    cy.url().should('match', /passenger|checkout|booking/i);
+  });
+});
