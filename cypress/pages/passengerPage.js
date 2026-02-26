09958d8a3059ab7d4c683063f3c4861aa5ee04bb
paolajpco <paolajpco@gmail.com>
2026-02-26 13:44:27 -0500

    fix: correct field mapping and class structure in PassengerPage
    fix: correct field mapping and class structure in PassengerPage

--- cypress/pages/passengerPage.js
+++ cypress/pages/passengerPage.js
@@ -1,59 +1,66 @@
 class PassengerPage {
+
   fillPassenger({
     sexo,
     nombres,
-    primer_apellido,
-    segundo_apellido,
+    apellidos,
     numero_documento,
     fecha_nacimiento,
-    nacionalidad
+    nacionalidad,
+    pais_residencia
   }) {
-    // Seleccionar el sexo (si aplica)
+
     if (sexo) {
-      cy.get('select[name*="gender"], [data-testid="gender"]').first()
+      cy.get('select[name*="gender"], [data-testid="gender"]')
+        .first()
         .select(sexo);
     }
 
-    // Nombres
     if (nombres) {
       cy.get('input[name*="first"], input[name*="name"], [data-testid="first-name"]')
-        .first().clear().type(nombres);
+        .first()
+        .clear()
+        .type(nombres);
     }
 
-    // Primer apellido
-    if (primer_apellido) {
+    if (apellidos) {
       cy.get('input[name*="last"], input[name*="surname"], [data-testid="last-name"]')
-        .first().clear().type(primer_apellido);
+        .first()
+        .clear()
+        .type(apellidos);
     }
 
-    // Segundo apellido
-    if (segundo_apellido) {
-      cy.get('input[name*="second"], [data-testid="second-last-name"]')
-        .first().clear().type(segundo_apellido);
-    }
-
-    // Número de documento o pasaporte
     if (numero_documento) {
       cy.get('input[name*="document"], input[name*="doc"], [data-testid="document"]')
-        .first().clear().type(numero_documento);
+        .first()
+        .clear()
+        .type(numero_documento);
     }
 
-    // Fecha de nacimiento
     if (fecha_nacimiento) {
       cy.get('input[type="date"], [data-testid="birth-date"]')
-        .first().clear().type(fecha_nacimiento);
+        .first()
+        .clear()
+        .type(fecha_nacimiento);
     }
 
-    // Nacionalidad
     if (nacionalidad) {
       cy.get('select[name*="nationality"], [data-testid="nationality"]')
-        .first().select(nacionalidad);
+        .first()
+        .select(nacionalidad);
+    }
+
+    if (pais_residencia) {
+      cy.get('[data-testid="residence-country"], select[name*=pais]')
+        .first()
+        .select(pais_residencia);
     }
   }
 
   continue() {
     cy.get('[data-testid="continue-button"], button[type="submit"], .continue')
-      .first().click();
+      .first()
+      .click();
   }
 }
 
